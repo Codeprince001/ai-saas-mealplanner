dbe59c387ea43ed83762b9d275e212193c388317
Codeprince001 <urangyorkwisdom@gmail.com>
2025-07-12 02:32:25 +0100

    feat: add Clerk middleware for route protection and subscription checks

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,16 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
 
-const isPublicRoute = createRouteMatcher(["/","/sign-up(.*)","/subscribe(.*)","/sign-in(.*)","/sign-out(.*)","/verify-email(.*)","/reset-password(.*)","/update-password(.*)","/update-profile(.*)","/terms-of-use(.*)","/privacy-policy(.*)"]);
+const isPublicRoute = createRouteMatcher(["/","/sign-up(.*)","/subscribe(.*)","/sign-in(.*)","/sign-out(.*)","/verify-email(.*)","/reset-password(.*)","/update-password(.*)","/update-profile(.*)","/terms-of-use(.*)","/privacy-policy(.*)","/webhook(.*)"]);
 
-const isSignUpRoute = createRouteMatcher(["/sign-up(.*)"])
-
-console.log("isSignUpRoute", isSignUpRoute);
+const isMealPlanRoute = createRouteMatcher(["/mealplan(.*)"])
 
 export default clerkMiddleware(async (auth, req) => {
+  // 🔁 Bypass Stripe webhook calls
+  if (req.nextUrl.pathname.startsWith("/api/check-subscription") || req.nextUrl.pathname.startsWith("/api/webhook")) {
+    console.log("Bypassing Clerk middleware for webhook route");
+    return NextResponse.next();
+  }
   // You can add custom logic here if needed ;
   const userAuth = await auth()
 
@@ -26,6 +29,20 @@ export default clerkMiddleware(async (auth, req) => {
     url.pathname = '/mealplan';
     url.search = '';
     return NextResponse.redirect(url);
+  }
+
+  if(isMealPlanRoute(req) && !userId) {
+    try {
+      const response = await fetch(`${req.nextUrl.origin}/api/check-subscription?userId=${userId}`);
+      const data = await response.json();
+
+      if (!data.subscriptionActive) {
+        return NextResponse.redirect(new URL('/subscribe', req.url));
+      }
+    }
+    catch (error) {
+      return NextResponse.redirect(new URL('/subscribe', req.url));
+    }
   }
 
   return NextResponse.next();
