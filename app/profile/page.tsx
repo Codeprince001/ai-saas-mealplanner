630b5fa0a2adf527f542a16e584367f5cdcdc3ad
Codeprince001 <urangyorkwisdom@gmail.com>
2025-07-19 13:37:38 +0100

    provide dummy card detail for users

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -139,6 +139,10 @@ const Profile = () => {
             <p className="text-gray-600 dark:text-gray-300 mt-1">
               {user.primaryEmailAddress?.emailAddress}
             </p>
+            <p className='text-gray-600 dark:text-gray-300 mt-1'>
+              Dummy Paster Card: 5555 5555 5555 4444
+            </p>
+            <p>Use any CVV or Expiring Date</p>
           </div>
         </div>
 
